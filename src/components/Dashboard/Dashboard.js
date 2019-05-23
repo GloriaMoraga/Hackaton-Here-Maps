6c0ecf5f7507d6a943a5a5d3a81272df49f86610
GloriaMoraga <silmeriel@gail.com>
2019-05-23 15:10:43 -0400

    agregando logo y favicon

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './dashboard.css';
 import Firebase from '../../firebaseConfig.js';
-import { Container, Col, Row, Image} from 'react-bootstrap';
+import { Container, Col, Row} from 'react-bootstrap';
 // import oniprofile from '../img/profile.png';
 // import stevenprofile from '../img/profile2.png';
 import Header from '../header/Header.js';
-import Footer from '../footer/Footer.js';
+
 
 class Dashboard extends Component {
   constructor(props) {
@@ -45,30 +45,16 @@ class Dashboard extends Component {
 
       <Header />
 
-        <Container >     
+        <Container >  
+        <Row>   
 
-          <Row mt-5>
-            <Col>
-             <div className="box-img-profile">
-                 <div className="profile-img">
-                     <h6>Steve</h6>
-                     {/* <div className="parent1"><div id="double-border1"/><Image id="login-steven1" src={stevenprofile} roundedCircle /></div> */}
-                     
-                     <p>10 Pts</p>
-                    
-                 </div>
-                 <div className="profile-img">
-                 <h6>Conie</h6>
-                 {/* <div className="parent1"><div id="double-border1"/><Image id="login-steven1" src={oniprofile} roundedCircle /></div> */}
-                
-                 <p>10 pts</p>
-
-                 </div>
-              </div>
+        <Col xs={12} md={12} lg={12} className="addWork mt-3" >
+           <div className="content-add">
+           <Link to="/create"><i class="fas fa-plus-circle icon-add"></i></Link>
+           </div>
            </Col>
-
-
-           <Col  xs={12} md={12} lg={12} className="tareas">
+           
+          <Col  xs={12} md={12} lg={12} className="tareas">
            <h4 className="text-center mt-2 mb-5">Tareas por hacer</h4>
 
            {this.state.boards.map(board =>
@@ -89,16 +75,12 @@ class Dashboard extends Component {
 
 
            
-           <Col xs={12} md={12} lg={12} className="addWork mt-3" >
-           <div className="content-add">
-           <Link to="/create"><i class="fas fa-plus-circle icon-add"></i></Link>
-           </div>
-           </Col>
+          
 
 
           </Row>
          </Container> 
-         <Footer />
+      
 
 
  </div>
