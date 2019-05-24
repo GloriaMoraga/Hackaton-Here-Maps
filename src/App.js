83896db35cae774099e86e0fb48afb66b620032c
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-05-23 22:24:06 -0400

    ya funcionan los marcadores

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Map from './components/mapas/Mapas.js';
-import Header from './components/header/Header.js';
-import Footer from './components/footer/Footer.js';
 import Firebase from './firebaseConfig';
-import Dashboard from './components/Dashboard/Dashboard';
-
-
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      lat: 52.530974,
-      lng: 13.384944, // Null Island
+      app_id: "GkUohGm3fVKilW9qMHcb",
+      app_code: "TXWMDz0kPxL0ZkRWCj-u5w",
+      lat: -33.453154, 
+      lng: -70.667730,
       error: null,
       user: {},
     }
 
   }
+  componentDidMount(){
+    if (navigator.geolocation) {
+     let ref = navigator.geolocation.getCurrentPosition(
+          (position) => {
+              this.setState({
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+              });
+          },
+          (error) => this.setState({
+              error: error.message
+          })
+      );
+      this.setState({ watchId: ref })
+  }
 
+  }
   authListener(){
     Firebase.auth().onAuthStateChanged((user)=>{
       if(user){
@@ -32,28 +44,21 @@ class App extends Component {
     });
    }
 
-render() {
-  return (
-   
+  render() {
+    return (
       <div className="App">
-      <Header /> 
         <Map
-          lat= { this.state.lat }
-          lng= { this.state.lng }
-          zoom="15"
-          theme="normal.day"
+        app_id={this.state.app_id}
+        app_code={this.state.app_code}
+        lat={this.state.lat}
+        lng={this.state.lng}
+        zoom='12'
+        theme="normal.day"
         />
-      <Footer />
- 
       </div>
-    )
-
-}  
+    );
+  }   
     }
 
 
 export default App;
-
-
-
-
