b456ff2cbcc9f2eb9a84cce263f78039ed051e68
GloriaMoraga <silmeriel@gail.com>
2019-05-24 07:12:44 -0400

    arreglando iconos, mejorando estructura

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import './dashboard.css';
 import Firebase from '../../firebaseConfig.js';
 import { Container, Col, Row, Card} from 'react-bootstrap';
 import btnuno from '../../images/botonuno.png';
+import pin from '../../images/location.png';
 import btndos from '../../images/botondos.png';
 import Header from '../header/Header.js';
 
@@ -73,12 +74,12 @@ class Dashboard extends Component {
                 <div key={i} className="card-box">
                   <Card className="card-box mt-2 mb-3" style={{ width: '100%' }}>
                   <Link to={`/show/${board.key}`}>
-                          <span className="badge badge-primary badge-pill ml-4 mt-3">{board.title}</span></Link>
+                          <span className="badge badge-success ml-1 mt-1">{board.title}</span></Link>
                     <Card.Img variant="top"src={board.url}  />
                     <Card.Body>
                       <Card.Title>{board.description}</Card.Title>
-                      <Card.Text>
-                        Usuario: {board.author}
+                      <Card.Text className="autor-class">
+                        <p>Usuario: {board.author}</p> <img className="pin" src={pin} alt="location"/>
                       </Card.Text>
                     </Card.Body>
                   </Card>
