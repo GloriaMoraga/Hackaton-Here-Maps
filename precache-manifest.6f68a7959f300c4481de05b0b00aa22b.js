107aaf367223adde8a2bfbf3296789a4d1a5e113
GloriaMoraga <silmeriel@gail.com>
2019-06-04 17:57:47 -0400

    Updates

---
+++ precache-manifest.6f68a7959f300c4481de05b0b00aa22b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751650232492ea7bf339fa5b3277732b",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/index.html"
+  },
+  {
+    "revision": "f7b1a6b1872fcc860751",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2e7c270a7b36e774df07",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/static/css/main.c39bee6c.chunk.css"
+  },
+  {
+    "revision": "f7b1a6b1872fcc860751",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/static/js/2.2b0ccc0c.chunk.js"
+  },
+  {
+    "revision": "2e7c270a7b36e774df07",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/static/js/main.b6921491.chunk.js"
+  },
+  {
+    "revision": "82ca90b030d578fa7891",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/static/js/runtime~main.b176dae7.js"
+  },
+  {
+    "revision": "57a4a6e8b099b4742e5a887f8fa34a60",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/static/media/botondos.57a4a6e8.png"
+  },
+  {
+    "revision": "c370386cb40f56f644186fab8361df30",
+    "url": "/GloriaMoraga/Hackaton-Here-Maps/static/media/botonuno.c370386c.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/GloriaMoraga/Hackaton-Here-Maps/precache-manifest.6f68a7959f300c4481de05b0b00aa22b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GloriaMoraga/Hackaton-Here-Maps/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
